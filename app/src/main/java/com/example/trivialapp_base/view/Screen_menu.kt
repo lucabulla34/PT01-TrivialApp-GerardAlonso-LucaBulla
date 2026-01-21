ad202a1730d770f39f556b229d06b28fefcf589c
AlPa1-dev <gerard.alonso.7e9@itb.cat>
2026-01-21 09:29:12 +0100

    Splash screen and dropdown menu

--- app/src/main/java/com/example/trivialapp_base/view/Screen_menu.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_menu.kt
@@ -1,18 +1,20 @@
 package com.example.trivialapp_base.view
-
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -22,25 +24,31 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import com.example.trivialapp_base.viewmodel.GameViewModel
-import com.example.trivialapp_base.R
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.trivialapp_base.R
+import com.example.trivialapp_base.viewmodel.GameViewModel
+
+
 @Composable
-fun MenuScreen(navController: NavController, viewModel: GameViewModel = viewModel()) {
-    val scrollState = rememberScrollState()
+fun MenuScreen(navController: NavController, viewModel: GameViewModel = viewModel(), modifier: Modifier = Modifier) {
+
+    var selectedText: String by remember { mutableStateOf("") }
+    var expanded: Boolean by remember { mutableStateOf(false) }
+    val difficulty = listOf("Easy", "Medium","El dark souls de los trivials")
+
 
     ConstraintLayout(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
-            .padding(top = 56.dp)
-            .verticalScroll(scrollState)
     ) {
-        val (logoRef) = createRefs()
+        val (logoRef, ddmRef) = createRefs()
 
         Image(
             painter = painterResource(id = R.drawable.logo),
@@ -49,20 +57,22 @@ fun MenuScreen(navController: NavController, viewModel: GameViewModel = viewMode
             modifier = Modifier
                 .size(100.dp)
                 .constrainAs(logoRef) {
-                    top.linkTo(parent.top, margin = 20.dp)
+                    top.linkTo(parent.top, margin = 100.dp)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
                 }
         )
-        
-}
-    @Composable
-    fun MyDropDownMenu() {
-        var selectedText by remember { mutableStateOf("") }
-        var expanded by remember { mutableStateOf(false) }
-        val difficulty = listOf("Facil", "Normal", "Dificil")
 
-        Column(Modifier.padding(20.dp)) {
+        Column(
+            Modifier
+                .padding(20.dp, 80.dp)
+                .constrainAs(ddmRef) {
+                    top.linkTo(parent.top, margin = 100.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+
+        ){
             OutlinedTextField(
                 value = selectedText,
                 onValueChange = { selectedText = it },
@@ -75,20 +85,19 @@ fun MenuScreen(navController: NavController, viewModel: GameViewModel = viewMode
             DropdownMenu(
                 expanded = expanded,
                 onDismissRequest = { expanded = false },
-                modifier = Modifier.fillMaxWidth()
-            ) {
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .border(1.dp, Color.Black, RoundedCornerShape(4.dp))
+            ){
                 difficulty.forEach { level ->
-                    DropdownMenuItem(text = { Text(text = level) },
+                    DropdownMenuItem(
+                        text = { Text(text = level) },
                         onClick = {
                             expanded = false
                             selectedText = level
                         })
                 }
             }
-
         }
-
-
     }
-
 }
